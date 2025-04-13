d9815fffeffa2cea616c41c387813ab02b93024a
Vitor Carvalho <vcml@pm.me>
2025-04-13 14:06:45 -0300

    adding one more form

--- themes/oquereluz/assets/js/submit-newsletter.js
+++ themes/oquereluz/assets/js/submit-newsletter.js
@@ -5,38 +5,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const myForm = event.target;
     const formData = new FormData(myForm);
     console.log(myForm);
-    console.log(formData.get("email"));
+    console.log(formData);
 
-    var substackURL = "https://oquereluz.substack.com/api/v1/free?email=" + encodeURIComponent(formData.get("email"));
-    console.log(substackURL);
-
-    fetch(substackURL, {
+    fetch("/", {
       method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" }
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString()
     })
-      .then(response => {
-        if (response.ok) {
-          console.log("deu tudo certo, veja lá");
-        } else {
-          console.log("algo deu errado, tem que ver lá");
+      .then(() => {
+        console.log("Form successfully submitted");
+        const modal = document.querySelector("#modal-newsletter-form-success")
+        if (modal) {
+          modal.classList.add("is-active");
+          modal.addEventListener("click", event => {
+            if (event.target.classList.contains("modal-background") || 
+                event.target.classList.contains("modal-close")) {
+                modal.classList.remove("is-active");
+            }
+          });
         }
       })
-      .catch(error => {
-        console.log("algo deu muito errado :(");
-      });
-    //     const modal = document.querySelector("#modal-contact-form-success")
-    //     if (modal) {
-    //       modal.classList.add("is-active");
-    //       modal.addEventListener("click", event => {
-    //         if (event.target.classList.contains("modal-background") || 
-    //             event.target.classList.contains("modal-close")) {
-    //             modal.classList.remove("is-active");
-    //         }
-    //       });
-    //     }
-    //   })
-    //   .catch(error => alert(error));
-  }; 
+      .catch(error => alert(error));
+  };
 
   document.getElementById("newsletter-form").addEventListener("submit", handleSubmit);
 });
